f9be703883f3a6f3fdda63ddaa67aa7c667c7504
Илья <ily470@yandex.ru>
2023-06-08 00:29:13 +0700

    0. Управление нитью через wait. [#6858 # [#6858 #421199]]

--- src/main/java/ru/job4j/CountBarrier.java
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -3,46 +3,45 @@ package ru.job4j;
 import javax.swing.text.TableView;
 
 public class CountBarrier {
-    private final Object monitor = this;
-
     private final int total;
 
-    private volatile int count = 0;
+    private int count = 0;
 
     public CountBarrier(final int total) {
         this.total = total;
     }
 
-    public void count() {
-        synchronized (monitor) {
-            count++;
-            this.notifyAll();
-        }
+    public synchronized void count() {
+        count++;
+        this.notifyAll();
     }
 
-    public void await() {
-        synchronized (monitor) {
-            while (this.count < this.total) {
-                try {
-                    this.wait();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
+    public synchronized void await() {
+        while (this.count < this.total) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         CountBarrier cb = new CountBarrier(10);
-        Thread thread = new Thread(
+        Thread thread1 = new Thread(
                 () -> {
                     cb.await();
                     System.out.println("конец");
                 }
         );
-        for (int i = 1; i < 11; i++) {
-            cb.count();
-        }
-        thread.start();
+        Thread thread2 = new Thread(
+                () -> {
+                    for (int i = 1; i < 11; i++) {
+                        cb.count();
+                    }
+                }
+        );
+        thread1.start();
+        thread2.start();
     }
 }
